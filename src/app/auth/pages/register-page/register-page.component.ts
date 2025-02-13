ceafd5522e111a28a0d469b63b3094470a5f5b82
Jordi Joan Bautista <jjoanbau@emeal.nttdata.com>
2025-02-13 15:54:21 +0100

    final commit

---
+++ src/app/auth/pages/register-page/register-page.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  templateUrl: './register-page.component.html',
+  standalone: false,
+  styles: [
+  ]
+})
+export class RegisterPageComponent {
+
+}
---
+++ src/app/reactive/pages/switches-page/switches-page.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  templateUrl: './switches-page.component.html',
+  standalone: false,
+  styles: [],
+})
+export class SwitchesPageComponent {}
